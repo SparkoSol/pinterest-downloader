19506669175c457d67ad7ed32a30c1ed366bcc9d
Rehman Ahmed Khan <72739116+rehmankha73@users.noreply.github.com>
2022-07-14 12:32:31 +0500

    initial config

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import VueAxios from "vue-axios";
+
+// importing styles
+import './styles.css'
+
+Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios)
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+
+    <div class="wrapper">
+      <h1 class="title">Pinterest Downloader</h1>
+      <form @submit.prevent="submitForm" ref="form">
+        <div>
+          <label for="url">
+            Video URL:
+            <input v-model="url" id="url"/>
+          </label>
+        </div>
+        <span v-if="error" style="color: red">{{ error }}</span>
+
+        <div>
+          <button class="btn" type="submit">Download</button>
+        </div>
+      </form>
+    </div>
+
+    <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      url: '',
+      error: '',
+    }
+  },
+  components: {
+    // HelloWorld
+  },
+  methods: {
+    submitForm() {
+      if(!this.url) {
+        this.error = 'Url is required!'
+        return;
+      }
+
+      console.log('Form submitted!')
+    }
+  }
+}
+</script>
