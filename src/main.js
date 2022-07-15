752a23b84a0e05266391a088eaae89ba421593ed
Rehman Ahmed Khan <72739116+rehmankha73@users.noreply.github.com>
2022-07-15 09:27:27 +0500

    Refactoring, adding axios, setting up initial functionality for sending request from front-end.

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
-import VueAxios from "vue-axios";
+// import VueAxios from "vue-axios";
 
 // importing styles
 import './assets/styles.css'
 
 Vue.config.productionTip = false
 
-Vue.use(VueAxios, axios)
+Vue.prototype.$axios = axios
 
 new Vue({
   router,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,37 @@
 <template>
-  <div class="home">
+  <div>
     <img alt="Vue logo" src="../assets/logo.png">
 
-    <div class="wrapper">
+    <div class="wrapper mb-5">
       <h1 class="title">Pinterest Downloader</h1>
-      <form @submit.prevent="submitForm" ref="form">
-        <div>
-          <label for="url">
-            Video URL:
-            <input v-model="url" id="url"/>
-          </label>
+
+      <form @submit.prevent="submitForm" class="py-2">
+        <div class="form-group">
+          <label for="url" class="d-block w-full text-left font-weight-bold">Video URL:</label>
+          <input v-model="url" type="text" class="form-control p-3" id="url" placeholder="Enter URL">
+          <small v-if="error" class="form-text text-danger">{{ error }}</small>
         </div>
-        <span v-if="error" style="color: red">{{ error }}</span>
 
         <div>
-          <button class="btn" type="submit">Download</button>
+          <button class="btn btn-primary" type="submit">Download</button>
         </div>
       </form>
+
+      <div class="d-flex flex-row">
+        <div class="mr-4">
+          <img src="../assets/images/market.jpg" alt="thumbnails" width="200" height="200"/>
+          <p class="form-text font-weight-bold">XYZ File</p>
+        </div>
+
+        <div class="border" style="width: 100%">
+          <div class="d-flex justify-content-between p-2" style="width: 100%" v-for="i in 5" :key="i">
+            <p>Format: <b>720.mp4</b> (Size: <b>2.8MB</b>)</p>
+            <button type="button" class="btn btn-sm btn-info">Download Now</button>
+          </div>
+        </div>
+
+
+      </div>
     </div>
 
     <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
@@ -24,28 +39,28 @@
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
+import { DownloadService } from "@/services/download-service";
 export default {
   name: 'HomeView',
   data() {
     return {
+      download_service: new DownloadService(),
       url: '',
       error: '',
     }
   },
-  components: {
-    // HelloWorld
-  },
+  components: {},
   methods: {
-    submitForm() {
-      if(!this.url) {
+    async submitForm() {
+      this.error = '';
+      if (!this.url) {
         this.error = 'Url is required!'
         return;
       }
 
+      console.log(this.url, 'url')
       console.log('Form submitted!')
+      await this.download_service.create(this.url)
     }
   }
 }
