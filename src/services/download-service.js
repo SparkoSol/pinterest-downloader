6325df653a37766b631749e226a9f5227c10adf7
Rehman Ahmed Khan <72739116+rehmankha73@users.noreply.github.com>
2022-07-15 09:28:27 +0500

    Adding download service class.

---
+++ src/services/download-service.js
@@ -0,0 +1,9 @@
+// import axios from 'axios';
+
+export class DownloadService {
+  async create(url) {
+    console.log(url, 'from service class')
+    // We will manage axios request here!
+    // return (await axios.post('/users', user)).data;
+  }
+}
